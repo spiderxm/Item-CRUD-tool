a244b4098d2ab3e4b6eeb81f4088caafd530b43f
spiderxm <mrigank.rkee@rediffmail.com>
2021-02-20 00:07:19 +0530

    Adding Functionality to delete item and find item by id and dump all items to json file

--- services/item_service.go
+++ services/item_service.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"gorm-practice/models"
+	"io/ioutil"
 	"os"
 )
 
@@ -52,13 +53,55 @@ func AllItems() {
 }
 
 func FindItem() {
-
+	var item models.Item
+	db := DBConn
+	var id int
+	fmt.Print("Enter id to find Record : ")
+	fmt.Scan(&id)
+	result := db.Find(&item, id)
+	if result.Error != nil {
+		fmt.Println("--- ", result.Error.Error(), " ---")
+	} else {
+		itemInJson, _ := json.MarshalIndent(item, "", "    ")
+		fmt.Println("--- Item Details in JSON ---")
+		fmt.Println(string(itemInJson))
+	}
 }
 
 func DeleteItem() {
+	db := DBConn
+	var id int
+	fmt.Print("Enter id to find Record : ")
+	fmt.Scan(&id)
+	var item models.Item
+	result := db.Find(&item, id)
+	if result.Error != nil {
+		fmt.Println("--- ", result.Error.Error(), " ---")
+	} else {
+		db.Delete(&item)
+		itemInJson, _ := json.MarshalIndent(item, "", "    ")
+		fmt.Println("--- Item Deleted Successfully ---")
+		fmt.Println("--- Item Details in JSON ---")
+		fmt.Println(string(itemInJson))
+	}
 
 }
 
 func AllItemsToJsonFile() {
-
+	var items []models.Item
+	db := DBConn
+	db.Find(&items)
+	itemsToJson, _ := json.MarshalIndent(items, "", "    ")
+	file, err := os.Create("items.json")
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		file.Close()
+		err = ioutil.WriteFile("items.json", itemsToJson, os.ModeAppend)
+		if err != nil {
+			fmt.Println(err.Error())
+		} else {
+			fmt.Println("--- Contents Written to File items.json---")
+		}
+	}
 }
